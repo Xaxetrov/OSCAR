3ef8a537d70632c6ef03f4c74c64e404bbf0b57d
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2017-10-12 11:28:32 +0200

    Added basic agent: select all marines & attack the closest visible zerg

--- src/AI.py
+++ src/AI.py
@@ -23,11 +23,6 @@ _SCREEN = [0]
 
 class FindAndDefeatZerglings():
 
-    targets = [None, None]
-    positions = [None, None]
-    selected = None
-    neutral_x, neutral_y, player_x, player_y = None, None, None, None
-
     def __init__(self):
         pass
 
@@ -36,11 +31,39 @@ class FindAndDefeatZerglings():
 
     def reset(self):
         pass
-        
-    def step(self, obs):
-        # Find our units and targets
-        player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-        self.neutral_y, self.neutral_x = (player_relative == _PLAYER_NEUTRAL).nonzero()
-        self.player_y, self.player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
 
-        return actions.FunctionCall(_NO_OP, [])
+    def step(self, obs):
+
+        # if some entities are selected
+        if _MOVE_SCREEN in obs.observation["available_actions"]:
+
+            # Find our units and targets
+            player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
+            ennemies_y, ennemies_x = (player_relative == _PLAYER_HOSTILE).nonzero()
+            player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
+
+            """print("ennemies_y: " + str(ennemies_y))
+            print("ennemies_x: " + str(ennemies_x))
+            print("player_y: " + str(player_y))
+            print("player_x: " + str(player_x))"""
+
+            # if no observations, do nothing
+            if not ennemies_y.any() or not player_y.any():
+                return actions.FunctionCall(_NO_OP, [])
+
+            player = [int(player_x.mean()), int(player_y.mean())]
+            
+            # compute closest ennemy
+            closest, min_dist = None, None
+            for p in zip(ennemies_x, ennemies_y):
+                dist = numpy.linalg.norm(numpy.array(player) - numpy.array(p))
+                if not min_dist or dist < min_dist:
+                    closest, min_dist = p, dist
+
+            time.sleep(0.4)
+            return actions.FunctionCall(_ATTACK_SCREEN, [_NOT_QUEUED, closest])
+            
+        else:
+            return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
+
+        
