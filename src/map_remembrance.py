f7569af7705f0cc329da9fa171401f6580294fc1
gegeAi <liermannjerome@gmail.com>
2017-10-22 18:06:16 +0200

    bit of refactor

--- src/map_remembrance.py
+++ src/map_remembrance.py
@@ -6,8 +6,12 @@ class MapRemembrance(object):
     __visible = 2
     __decay_factor = 1.01
     
-    def __init__(self, size):
+    def __init__(self, size, decaying_function=None):
         self.__map = np.zeros((size[0], size[1], 2))
+        if decaying_function is not None:
+            self.decaying_function = decaying_function
+        else:
+            self.decaying_function = lambda x: x/self.__decay_factor
         
     def maj_informations(self, minimap_informations, visible_parts):
         indices = np.where(visible_parts == self.__visible)
@@ -15,7 +19,7 @@ class MapRemembrance(object):
         
         self.__map[:, :, 0][indices] = minimap_informations[indices]
         self.__map[:, :, 1][indices] = 1
-        self.__map[:, :, 1][inverse_indices] /= self.__decay_factor
+        self.__map[:, :, 1][inverse_indices] = self.decaying_function(self.__map[:, :, 1][inverse_indices])
         
     def get_most_recent_informations(self, layer, value):
         indices = np.where(self.__map[:, :, layer] == value)
@@ -35,3 +39,22 @@ class MapRemembrance(object):
             coordinates = np.ndarray(0)
         
         return coordinates, proba
+    
+	#here to be pushed, but meant to be used in AI. not tested yet
+	def __scout(self, obs):
+        # check on the probability layer to access never seen zones
+        black = self.memory.get_most_recent_informations(1, 0)[0]
+        if len(black) == 0:
+            return actions.FunctionCall(_NO_OP, [])
+            
+        player_relative = obs.observation["minimap"][_PLAYER_RELATIVE]
+        player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
+        player = [int(player_x.mean()), int(player_y.mean())]
+        
+        # compute closest not seen point
+        closest, min_dist = None, None
+        for p in black:
+            dist = np.linalg.norm(np.array(player) - np.array(p))
+            if not min_dist or dist < min_dist:
+                closest, min_dist = p, dist
+    
