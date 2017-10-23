c580210804258a882591ea37d7759ef8723abbe1
Six Nicolas <nsix@IF501-219-23.insa-lyon.fr>
2017-10-23 11:24:34 +0200

    update launch script

--- rl_run.sh
+++ rl_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PYTHONPATH=$(pwd)
+PYTHONPATH=$(pwd):$PYTHONPATH
 export PYTHONPATH
 
 python3.6 oscar/RlDqnAgent.py --mode train --memory-size 40000 --model Neuralnetwork/DenseMineralShard.knn --outmodel Neuralnetwork/DenseMineralShard.knn --trainstep 300000
