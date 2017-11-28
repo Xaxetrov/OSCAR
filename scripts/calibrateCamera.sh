98eff1b458490b7214c87bce91c88f16c9628c28
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2017-11-28 14:27:50 +0100

    Added calibration and coordinates helper

---
+++ scripts/calibrateCamera.sh
@@ -0,0 +1,13 @@
+# Calibrates camera for map given as parameter and exports result.
+# Example usage: "sh calibrateCamera.sh Flat64"
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit
+fi
+
+cd ..
+mkdir -p ./data/tmp
+echo "$1" > data/tmp/map_name.txt
+python3 -m pysc2.bin.agent --map $1 --agent oscar.util.coordinatesHelper.Calibration
