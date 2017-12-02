a4764f76d043352a3e1e060f609fb9bb81ca3c4c
Nicolas <nicolas.six@pau.fr>
2017-12-02 10:51:17 +0100

    normalization of output of env perceptron simple64

--- oscar/env/envs/pysc2_simple64_meta_per_env.py
+++ oscar/env/envs/pysc2_simple64_meta_per_env.py
@@ -10,16 +10,22 @@ class Pysc2Simple64MetaPerEnv(Pysc2Simple64MetaEnv):
 
     observation_space = spaces.Box(low=0, high=float('Inf'), shape=(10,))
 
+    def __init__(self):
+        super().__init__()
+        self.observation_space.n = self.observation_space.shape[0]
+
     def get_return_obs(self, currant_obs):
         unit_type = currant_obs.observation["screen"][SCREEN_UNIT_TYPE]
         ret_obs_list = deque()
-        ret_obs_list.append(currant_obs.observation['player'][MINERALS])
-        ret_obs_list.append(currant_obs.observation['player'][VESPENE])
-        ret_obs_list.append(currant_obs.observation['player'][FOOD_USED])
-        ret_obs_list.append(currant_obs.observation['player'][FOOD_USED_BY_ARMY])
-        ret_obs_list.append(currant_obs.observation['player'][FOOD_USED_BY_WORKERS])
-        ret_obs_list.append(currant_obs.observation['player'][FOOD_CAP])
-        ret_obs_list.append(currant_obs.observation['player'][ARMY_COUNT])
+        minerals = currant_obs.observation['player'][MINERALS] / 1000.0
+        ret_obs_list.append(min(minerals, 1.0))
+        vespene = currant_obs.observation['player'][VESPENE] / 1000.0
+        ret_obs_list.append(min(vespene, 1.0))
+        ret_obs_list.append(currant_obs.observation['player'][FOOD_USED] / 200.0)
+        ret_obs_list.append(currant_obs.observation['player'][FOOD_USED_BY_ARMY] / 200.0)
+        ret_obs_list.append(currant_obs.observation['player'][FOOD_USED_BY_WORKERS] / 200.0)
+        ret_obs_list.append(currant_obs.observation['player'][FOOD_CAP] / 200.0)
+        ret_obs_list.append(currant_obs.observation['player'][ARMY_COUNT] / 100.0)
         ret_obs_list.append(np.count_nonzero(unit_type == TERRAN_BARRACKS_ID) > 0)
         ret_obs_list.append(np.count_nonzero(unit_type == TERRAN_SUPPLYDEPOT) > 0)
         selected_unit_id = currant_obs.observation['single_select'][0][0]
@@ -30,8 +36,8 @@ class Pysc2Simple64MetaPerEnv(Pysc2Simple64MetaEnv):
                 pass
             except IndexError:
                 pass
-        ret_obs_list.append(selected_unit_id)
-        return np.array(ret_obs_list, copy=True)
+        ret_obs_list.append(selected_unit_id / TERRAN_MARINE)
+        return np.array(ret_obs_list, copy=True, dtype=float)
 
     @staticmethod
     def get_action_id_from_action(sc2_action, sc2_args):
