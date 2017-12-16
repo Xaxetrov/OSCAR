042b3021d7e64534b84863fa5934ebf950b4c17c
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2017-12-16 14:58:44 +0100

    Completed implementation

--- oscar/util/get_centroids.py
+++ oscar/util/get_centroids.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import floor
 
 # start points for matching
 _TOP_LEFT = 0
@@ -53,7 +54,7 @@ def _move_to_foreground(player_relative, unit_density, unit_id):
 					process_pixel(cur[0], cur[1] + 1, player_relative, unit_density, processed, unit_id, stack)
 
 
-# Builds a disk mask using midpoint circle algorithm
+""" Builds a disk mask using midpoint circle algorithm """
 def _get_disk_mask(radius):
 
 	def draw_hor_line(mat, x0, x1, y):
@@ -126,24 +127,28 @@ unit_id -- the unit id to be matched in player_relative
 direction -- direction used for matching (_TOP_LEFT, _TOP_RIGHT, _BOTTOM_LEFT or _BOTTOM_RIGHT)
 start_point -- start point of the matching
 """
-def _is_match(player_relative, mask, unit_id, direction, start_point):
+def _is_match(player_relative, unit_density, mask, unit_id, direction, start_point):
 	start_x, start_y, end_x, end_y, step_x, step_y = \
 		_get_match_start_end_step(direction, (len(mask), len(mask[0])))
 
-	for i in range(start_x, end_x, step_x):
-		for j in range(start_y, end_y, step_y):
-			abs_x = start_point[0] + i*step_x
-			abs_y = start_point[1] + j*step_y
+	pixels_matched = 0
+
+	for x in range(start_x, end_x, step_x):
+		for y in range(start_y, end_y, step_y):
+			abs_x = start_point[0] + x*step_x
+			abs_y = start_point[1] + y*step_y
 
 			if abs_x < 0 or abs_x >= len(player_relative) \
 				or abs_y < 0 or abs_y >= len(player_relative[0]):
 				continue
 
-			if mask[i, j] != 0 and \
-				player_relative[abs_x, abs_y] != unit_id:
+			if mask[x, y] != 0 and \
+				(unit_density[abs_x, abs_y] <= 0 or player_relative[abs_x, abs_y] != unit_id):
 				return False
+			elif mask[x, y] != 0:
+				pixels_matched += 1
 
-	return True
+	return (pixels_matched > 0)
 
 
 """
@@ -171,7 +176,79 @@ def _update_unit_density(unit_density, mask, direction, start_point):
 
 
 """
+Given coordinates of a corner of the mask,
+returns mask center
+"""
+def _get_mask_center(x, y, radius, direction):
+	if direction == _TOP_LEFT:
+		return (x - radius, y - radius)
+	elif direction == _TOP_RIGHT:
+		return (x + radius, y - radius)
+	elif direction == _BOTTOM_LEFT:
+		return (x - radius, y + radius)
+	else: # _BOTTOM_RIGHT
+		return (x + radius, y + radius)
+
+
+"""
+Computes and returns disks centers positions by matching them with a mask,
+for a given quarter (_TOP_LEFT, _TOP_RIGHT, _BOTTOM_LEFT, _BOTTOM_RIGHT)
+"""
+def _get_centroid_centers_quarter(player_relative, unit_density, disks_radius, mask, unit_id, quarter):
+	centroids = []
+
+	step_x, step_y, start_x, start_y, end_x, end_y = None, None, None, None, None, None
+	width, height = len(player_relative), len(player_relative[0])
+
+	if quarter == _TOP_LEFT:
+		step_x, step_y = -1, -1
+		start_x, end_x = floor(width/2), -1
+		start_y, end_y = floor(height/2), -1
+	elif quarter == _TOP_RIGHT:
+		step_x, step_y = 1, -1
+		start_x, end_x = floor(width/2)+1, width
+		start_y, end_y = floor(height/2), -1
+	elif quarter == _BOTTOM_LEFT:
+		step_x, step_y = -1, 1
+		start_x, end_x = floor(width/2), -1
+		start_y, end_y = floor(height/2)+1, height
+	else: # BOTTOM_RIGHT
+		step_x, step_y = 1, 1
+		start_x, end_x = floor(width/2)+1, width
+		start_y, end_y = floor(height/2)+1, height
+
+	for x in range(start_x, end_x, step_x):
+		for y in range(start_y, end_y, step_y):
+			if _is_match(player_relative, unit_density, mask, unit_id, quarter, (x, y)):
+				centroids.append(_get_mask_center(x, y, disks_radius, quarter))
+				_update_unit_density(unit_density, mask, quarter, (x, y))
+
+	return centroids
+
+
+"""
 Computes and returns disks centers positions by matching them with a mask.
+Handles disks partially outside of the screen as well.
+When there are multiple possible matching, chooses a possibility which minimizes the number of disks.
+
+Requirement: disks_radius should be smaller than half of the width and the height of the screen.
+
+Keyword arguments:
+player_relative -- player_relative observation layer
+unit_density -- unit_density observation layer
+disks_radius -- radius of disks to match
+unit_id -- the unit id to be matched in player_relative
 """
 def get_centroid_centers(player_relative, unit_density, disks_radius, unit_id):
-	return
+	assert disks_radius < len(player_relative)/2 and disks_radius < len(player_relative[0])/2, \
+		"Error: disks_radius should be smaller than half of the width and the height of the screen"
+
+	mask = _get_disk_mask(disks_radius)
+	_move_to_foreground(player_relative, unit_density, unit_id)
+
+	centroids = _get_centroid_centers_quarter(player_relative, unit_density, disks_radius, mask, unit_id, _TOP_LEFT)
+	centroids += _get_centroid_centers_quarter(player_relative, unit_density, disks_radius, mask, unit_id, _TOP_RIGHT)
+	centroids += _get_centroid_centers_quarter(player_relative, unit_density, disks_radius, mask, unit_id, _BOTTOM_LEFT)
+	centroids += _get_centroid_centers_quarter(player_relative, unit_density, disks_radius, mask, unit_id, _BOTTOM_RIGHT)
+
+	return centroids
