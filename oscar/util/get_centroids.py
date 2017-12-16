d702c93983b4dc7f581234b8cb319572e0686e2e
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2017-12-16 12:09:15 +0100

    Implemented functions for mask matching

--- oscar/util/get_centroids.py
+++ oscar/util/get_centroids.py
@@ -87,6 +87,36 @@ def _get_disk_mask(radius):
 	return mask
 
 
+def _get_match_start_end_step(direction, mask_size):
+	# Gets step depending on direction
+	step_x, step_y = None, None
+	if direction == _TOP_LEFT:
+		step_x, step_y = -1, -1
+	elif direction == _TOP_RIGHT:
+		step_x, step_y = 1, -1
+	elif direction == _BOTTOM_LEFT:
+		step_x, step_y = -1, 1
+	else: # _BOTTOM_RIGHT
+		step_x, step_y = 1, 1
+
+	# Gets start and end boudaries using steps
+	start_x, start_y, end_x, end_y = None, None, None, None
+	if step_x == 1:
+		start_x = 0
+		end_x = mask_size[0]
+	else: # step_x == -1
+		start_x = mask_size[0] - 1
+		end_x = -1
+	if step_y == 1:
+		start_y = 0
+		end_y = mask_size[1]
+	else: # step_y == -1
+		start_y = mask_size[1] - 1
+		end_y = -1
+
+	return start_x, start_y, end_x, end_y, step_x, step_y
+
+
 """
 Returns whether their is a match between the mask and the player_relative layer.
 Returns true even if the match is partially outside of the layer.
@@ -97,19 +127,47 @@ direction -- direction used for matching (_TOP_LEFT, _TOP_RIGHT, _BOTTOM_LEFT or
 start_point -- start point of the matching
 """
 def _is_match(player_relative, mask, unit_id, direction, start_point):
-	return false
+	start_x, start_y, end_x, end_y, step_x, step_y = \
+		_get_match_start_end_step(direction, (len(mask), len(mask[0])))
+
+	for i in range(start_x, end_x, step_x):
+		for j in range(start_y, end_y, step_y):
+			abs_x = start_point[0] + i*step_x
+			abs_y = start_point[1] + j*step_y
+
+			if abs_x < 0 or abs_x >= len(player_relative) \
+				or abs_y < 0 or abs_y >= len(player_relative[0]):
+				continue
+
+			if mask[i, j] != 0 and \
+				player_relative[abs_x, abs_y] != unit_id:
+				return False
+
+	return True
 
 
 """
 Updates unit_density by decrementing cells matching with the mask.
 
 Keyword arguments:
-unit_id -- the unit id to be matched in player_relative
 direction -- direction used for matching (_TOP_LEFT, _TOP_RIGHT, _BOTTOM_LEFT or _BOTTOM_RIGHT)
 start_point -- start point of the matching
 """
-def _update_unit_density(unit_density, mask, unit_id, direction, start_point):
-	return
+def _update_unit_density(unit_density, mask, direction, start_point):
+	start_x, start_y, end_x, end_y, step_x, step_y = \
+		_get_match_start_end_step(direction, (len(mask), len(mask[0])))
+
+	for i in range(start_x, end_x, step_x):
+		for j in range(start_y, end_y, step_y):
+			abs_x = start_point[0] + i*step_x
+			abs_y = start_point[1] + j*step_y
+
+			if abs_x < 0 or abs_x >= len(unit_density) \
+				or abs_y < 0 or abs_y >= len(unit_density[0]):
+				continue
+
+			if mask[i, j] != 0:
+				unit_density[abs_x, abs_y] -= 1
 
 
 """
