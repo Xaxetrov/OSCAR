95b95081068e9f61d2ad0645444ebfbff7cf27f6
Nicolas <nicolas.six@pau.fr>
2018-01-13 20:41:42 +0100

    Update README and add comments

--- oscar/agent/learning_agent.py
+++ oscar/agent/learning_agent.py
@@ -5,9 +5,23 @@ from oscar.agent.custom_agent import CustomAgent
 class LearningAgent(LearningStructure, CustomAgent):
     """
     An abstract class to build learning agent
+
+    Sub class must implement the following LearningStructure methods:
+      - _step
+      - _format_observation
+      - _transform_action
+    (see LearningStructure code for more information on them)
     """
 
     def __init__(self, train_mode=False, shared_memory=None):
+        """
+        Constructor of the abstract class LearningAgent
+        Sub class must keep the same calling format to work with the hierarchy factory
+        self.observation_space and self.action_space must be set before calling this constructor
+        :param train_mode: if the agent must train or play (default: False -> play)
+        :param shared_memory: the memory used during training to communicate with the environment
+                    (useless when playing, indispensable when training)
+        """
         LearningStructure.__init__(self, train_mode, shared_memory)
         self.failed_meta_action_counter = 0
 
