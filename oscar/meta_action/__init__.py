1ec61f41da22d4266e250918fe97352ca87a1a21
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2018-01-16 23:56:57 +0100

    Removed useless meta-action (camera)

--- oscar/meta_action/__init__.py
+++ oscar/meta_action/__init__.py
@@ -2,7 +2,6 @@ from oscar.meta_action.meta_action_error import *
 from oscar.meta_action.build import *
 from oscar.meta_action.select import *
 from oscar.meta_action.train_unit import *
-from oscar.meta_action.camera import *
 from oscar.meta_action.scout import *
 from oscar.meta_action.spy import *
 from oscar.meta_action.attack import *
